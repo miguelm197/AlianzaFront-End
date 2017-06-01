dbdb688d15afa4a07886efee8b852c9795cffdf8
miguelm197 <miguelmax197@gmail.com>
2017-06-01 19:41:47 -0300

    Clase

---
+++ Alianza/Ejemplos/Miguel Merelli/Sitio2/example4.js
@@ -0,0 +1,23 @@
+var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('comando> ');
+rl.prompt();
+rl.on('line', function (line) {
+    //    console.log('ejecute funcion')
+    if (line == "init") {
+        console.log('... Iniciando máquina ...')
+    } else if (line == 'hola') {
+        console.log("maquina dice: hola humano")
+    } else if (line == 'adios') {
+        console.log("maquina dice: adios humano")
+    } else if (line == 'comandos') {
+        console.log("-init\n-hola\n-adios\n-comandos\n-exit")
+    } else if (line == 'exit') {
+        rl.close();
+    } else
+        console.log("Comando incorrecto...")
+
+    rl.prompt();
+}).on('close', function () {
+    process.exit(0);
+});
