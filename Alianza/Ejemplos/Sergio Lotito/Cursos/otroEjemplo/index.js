243891fe0d111fbb3503346f316067fe8f28cd36
slotito <selorigi@gmail.com>
2017-06-27 22:27:35 -0300

    subida

---
+++ Alianza/Ejemplos/Sergio Lotito/Cursos/otroEjemplo/index.js
@@ -0,0 +1,13 @@
+fs = require('fs');
+
+//console.log('Hola soy un ejemplo')
+fs.readFile('D:\Curso_Front-End\Cursos\otroEjemplo\archivos.txt', 'utf-8', leerDatos);
+
+
+function leerDatos(error, data) {
+    if (error)
+        return console.log(error);
+        
+    console.log('data:', data);
+    console.log('error:', error);
+}
