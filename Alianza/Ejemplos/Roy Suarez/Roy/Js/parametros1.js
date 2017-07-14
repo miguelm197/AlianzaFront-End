96a0dafb42f468546befd3886ae36749a26d6878
Roysuarez <roysuarez94@gmail.com>
2017-07-13 21:14:42 -0300

    De todo

---
+++ Alianza/Ejemplos/Roy Suarez/Roy/Js/parametros1.js
@@ -0,0 +1,10 @@
+fs = require('fs');
+
+console.log('Hola soy un ejemplo');
+fs.readFile('archivos.txt', 'utf-8', leerDatos);
+
+function leerDatos(error, data) {
+    if (error)
+        return console.log(error);
+    console.log('data:', data);
+}
